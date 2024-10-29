8a6ab69c98503961b47042ce110d1972ef57fa09
Cleidssom <Cleidssom01@gmail.com>
2024-10-29 09:51:45 -0300

    .fhfdg

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ServicesModule } from './services/services.module';
 import { UtilityModule } from './utility/utility.module';
 
 @Module({
-  imports: [ServicesModule, UtilityModule],
+  imports: [ UtilityModule],
   controllers: [AppController],
   providers: [AppService],
 })
