5ba6811c8d076272d9b4660fd772bf85455d4513
Cleidssom <Cleidssom01@gmail.com>
2024-11-05 07:18:58 -0300

    feat: Create endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UtilityModule } from './utility/utility.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ UtilityModule],
+  imports: [UtilityModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
